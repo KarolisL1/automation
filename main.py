2942dea4c9a820e1164a3e892b2958bb21e9a406
karolisliepa <Karolis.Liepa@gmail.com>
2021-09-01 16:39:13 -0400

    Moving.

--- main.py
+++ main.py
@@ -1,14 +1,27 @@
 import glob
 import shutil
+import os
 
-# I prefer to set path and mask as variables, but of course you can use values
-# inside glob() and move()
+filetype = input("What's the file type? ").lower()
 
-source_files='/Users/karolisliepa/Downloads/*.avi'
-target_folder='/Users/karolisliepa/Desktop/Filmai'
+dirname = input("Where you want to move the files? ")
+path = "/Users/karolisliepa"
+path2 = "/Users/karolisliepa/Desktop/"
+isExist = os.path.exists(path2)
+
+directory = os.path.join(path2, dirname)
+
+if isExist:
+    print("Directory exist.")
+else:
+    os.mkdir(directory, 0o0666)
+
+
+source_files=f"/Users/karolisliepa/Downloads/*.{filetype}"
+target_folder=f"/Users/karolisliepa/Desktop/{dirname}"
 
 # retrieve file list
-filelist=glob.glob(source_files)
+filelist = glob.glob(source_files)
 for single_file in filelist:
      # move file with full paths as shutil.move() parameters
     shutil.move(single_file,target_folder)
